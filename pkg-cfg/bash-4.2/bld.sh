c0f1c1ae139419ef65a6c56cf976bde94e2d5271
djerome <douglas@ttylinux.org>
2013-06-23 13:20:39 -0700

    start working the package building, no x-tools yet

--- pkg-cfg/bash-4.2/bld.sh
+++ pkg-cfg/bash-4.2/bld.sh
@@ -1,2 +1,95 @@
+#!/bin/${cl_bash}
+
+
+# This file is part of the crosslinux software.
+# The license which this software falls under is GPLv2 as follows:
+#
+# Copyright (C) 2013-2013 Douglas Jerome <douglas@crosslinux.org>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+# ******************************************************************************
+# Definitions
+# ******************************************************************************
+
 PKG_URL="http://ftp.gnu.org/gnu/bash/"
 PKG_ZIP="bash-4.2.tar.gz"
+PKG_SUM=""
+
+PKG_TAR="bash-4.2.tar"
+PKG_DIR="bash-4.2"
+
+
+# ******************************************************************************
+# pkg_patch
+# ******************************************************************************
+
+pkg_patch() {
+PKG_STATUS=""
+return 0
+}
+
+
+# ******************************************************************************
+# pkg_configure
+# ******************************************************************************
+
+pkg_configure() {
+PKG_STATUS=""
+return 0
+}
+
+
+# ******************************************************************************
+# pkg_make
+# ******************************************************************************
+
+pkg_make() {
+PKG_STATUS=""
+return 0
+}
+
+
+# ******************************************************************************
+# pkg_install
+# ******************************************************************************
+
+pkg_install() {
+
+PKG_STATUS="install error"
+
+if [[ -d "rootfs/" ]]; then
+	${cl_find} "rootfs/" ! -type d -exec touch {} \;
+	${cl_fakeroot} -- cp --archive --force rootfs/* "${TARGET_SYSROOT_DIR}"
+fi
+
+PKG_STATUS=""
+return 0
+
+}
+
+
+# ******************************************************************************
+# pkg_clean
+# ******************************************************************************
+
+pkg_clean() {
+PKG_STATUS=""
+return 0
+}
+
+
+# end of file
