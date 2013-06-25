e242dd47dad6502dac6d9c87e687f5c68f2d4542
djerome <douglas@ttylinux.org>
2013-06-25 16:24:30 -0700

    fix ncurses and libc building process

--- pkg-cfg/crosslinux-libc/bld.sh
+++ pkg-cfg/crosslinux-libc/bld.sh
@@ -69,20 +69,56 @@ return 0
 
 pkg_install() {
 
-local srcdir="${CONFIG_XBT_SYSROOT_DIR}"
-local bf="etc/${CONFIG_BRAND_NAME}-build"
-local tf="etc/${CONFIG_BRAND_NAME}-target"
+local    srcdir="${CONFIG_XBT_SYSROOT_DIR}"
+local    srcname=""
+local    dstfile=""
+local    dstpath=""
+local -i pitchit=0
 
 PKG_STATUS="install error"
 
 CL_logcom "Copying cross-tool target components to sysroot."
-cp --no-dereference --recursive "${srcdir}"/* "${TARGET_SYSROOT_DIR}"
 
+# What a pain.  I want this:
+# cp --no-dereference --recursive "${srcdir}"/* "${TARGET_SYSROOT_DIR}"
+#
+# But the source files may be read-only, and I do not want read-only files in
+# the project sysroot.  Also, there may be some directory symlinks which I do
+# not want.
+#
+# The following gyrations find all the non-directory files from the cross-tool
+# chain sysroot, filters the directory symlinks, makes any needed directory
+# path, copies the file to the project sysroot, then adds group and user write
+# and execute permissions if the file is not a symlink.
+#
+${cl_find} "${srcdir}" ! -type d | while read srcname; do
+	CL_logcom "~~~~~ ${srcname#${srcdir}/}"
+	pitchit=0
+	if [[ -L "${srcname}" ]]; then
+		[[ -d "$(readlink -f ${srcname})" ]] && pitchit=1 || true
+	fi
+	if [[ ${pitchit} -eq 0 ]]; then
+		dstfile="${TARGET_SYSROOT_DIR}/${srcname#${srcdir}/}"
+		dstpath="${dstfile%/*}"
+		if [[ ! -d "${dstpath}" ]]; then
+			mkdir --mode=755 --parents --verbose "${dstpath}"
+		fi
+		cp --archive --verbose "${srcname}" "${dstpath}"
+		[[ ! -L "${dstfile}" ]] && chmod gu+wx "${dstfile}" || true
+	else
+		CL_logcom "~~~~~ PITCH IT ^" # (directory symlink)
+	fi
+done
+
+# Get the typical pkg-cfg rootfs/ files.
+#
 if [[ -d "rootfs/" ]]; then
 	${cl_find} "rootfs/" ! -type d -exec touch {} \;
 	cp --archive --force rootfs/* "${TARGET_SYSROOT_DIR}"
 fi
 
+local bf="etc/${CONFIG_BRAND_NAME}-build"
+local tf="etc/${CONFIG_BRAND_NAME}-target"
 CL_logcom "Recording build information in the target:"
 CL_logcom "=> /${bf}"
 CL_logcom "=> /${tf}"
