7a25b0184082b1397de5ad94448aafe15413b5fa
djerome <douglas@ttylinux.org>
2013-06-24 21:59:21 -0700

    added ncurses build script

--- pkg-cfg/ncurses-5.9/bld.sh
+++ pkg-cfg/ncurses-5.9/bld.sh
@@ -48,8 +48,56 @@ return 0
 # ******************************************************************************
 
 pkg_configure() {
+
+local ENABLE_WIDEC=""
+
+PKG_STATUS="./configure error"
+
+cd "${PKG_DIR}"
+
+if [[ x"${CONFIG_BLD_NCURSES_WIDEC:-}" == x"y" ]]; then
+	ENABLE_WIDEC="--enable-widec"
+fi
+
+mv -v misc/terminfo.src misc/terminfo.src-ORIG
+cp -v ${CROSSLINUX_PKGCFG_DIR}/$1/terminfo.src misc/terminfo.src
+
+PATH="${CONFIG_XBT_DIR}:${PATH}" \
+AR="${CONFIG_XBT_NAME}-ar" \
+AS="${CONFIG_XBT_NAME}-as --sysroot=${TARGET_SYSROOT_DIR}" \
+CC="${CONFIG_XBT_NAME}-cc --sysroot=${TARGET_SYSROOT_DIR}" \
+CXX="${CONFIG_XBT_NAME}-c++ --sysroot=${TARGET_SYSROOT_DIR}" \
+LD="${CONFIG_XBT_NAME}-ld --sysroot=${TARGET_SYSROOT_DIR}" \
+NM="${CONFIG_XBT_NAME}-nm" \
+OBJCOPY="${CONFIG_XBT_NAME}-objcopy" \
+RANLIB="${CONFIG_XBT_NAME}-ranlib" \
+SIZE="${CONFIG_XBT_NAME}-size" \
+STRIP="${CONFIG_XBT_NAME}-strip" \
+CFLAGS="${CONFIG_CFLAGS}" \
+./configure \
+	--build=${MACHTYPE} \
+	--host=${CONFIG_XBT_NAME} \
+	--prefix=/usr \
+	--libdir=/lib \
+	--mandir=/usr/share/man \
+	--enable-shared \
+	--enable-overwrite \
+	${ENABLE_WIDEC} \
+	--disable-largefile \
+	--disable-termcap \
+	--with-build-cc=gcc \
+	--with-install-prefix=${TARGET_SYSROOT_DIR} \
+	--with-shared \
+	--without-ada \
+	--without-debug \
+	--without-gpm \
+	--without-normal || return 1
+
+cd ..
+
 PKG_STATUS=""
 return 0
+
 }
 
 
@@ -58,8 +106,18 @@ return 0
 # ******************************************************************************
 
 pkg_make() {
+
+PKG_STATUS="make error"
+
+cd "${PKG_DIR}"
+PATH="${CONFIG_XBT_DIR}:${PATH}" make \
+	--jobs=${NJOBS} \
+	CROSS_COMPILE=${CONFIG_XBT_NAME}- || return 1
+cd ..
+
 PKG_STATUS=""
 return 0
+
 }
 
 
@@ -70,6 +128,10 @@ return 0
 pkg_install() {
 
 PKG_STATUS="install error"
+
+cd "${PKG_DIR}"
+PATH="${CONFIG_XBT_DIR}:${PATH}" make install || return 1
+cd ..
 
 if [[ -d "rootfs/" ]]; then
 	${cl_find} "rootfs/" ! -type d -exec touch {} \;
