f5230f130d5047361b691ffa10f6e7aeab26d634
djerome <douglas@ttylinux.org>
2015-01-14 18:29:31 -0700

    rename x86 family and add tree-1.7.0

---
+++ pkg-cfg/tree-1.7.0/bld.sh
@@ -0,0 +1,129 @@
+#!/bin/${cl_bash}
+
+
+# This file is part of the crosslinux software.
+# The license which this software falls under is GPLv2 as follows:
+#
+# Copyright (C) 2013-2013 Douglas Jerome <djerome@crosslinux.org>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+# ******************************************************************************
+# Definitions
+# ******************************************************************************
+
+PKG_URL="ftp://mama.indstate.edu/linux/tree/"
+PKG_ZIP="tree-1.7.0.tgz"
+PKG_SUM=""
+
+PKG_TAR="tree-1.7.0.tar"
+PKG_DIR="tree-1.7.0"
+
+
+# ******************************************************************************
+# pkg_patch
+# ******************************************************************************
+
+pkg_patch() {
+
+local patchDir="${CROSSLINUX_PKGCFG_DIR}/$1/patch"
+local patchFile=""
+
+PKG_STATUS="patch error"
+
+cd "${PKG_DIR}"
+for patchFile in "${patchDir}"/*; do
+	[[ -r "${patchFile}" ]] && patch -p1 <"${patchFile}"
+done
+cd ..
+
+PKG_STATUS=""
+return 0
+
+}
+
+
+# ******************************************************************************
+# pkg_configure
+# ******************************************************************************
+
+pkg_configure() {
+PKG_STATUS=""
+return 0
+}
+
+
+# ******************************************************************************
+# pkg_make
+# ******************************************************************************
+
+pkg_make() {
+
+PKG_STATUS="make error"
+
+cd "${PKG_DIR}"
+source "${CROSSLINUX_SCRIPT_DIR}/_xbt_env_set"
+PATH="${CONFIG_XTOOL_BIN_DIR}:${PATH}" make \
+	--jobs=${NJOBS} \
+	CROSS_COMPILE=${CONFIG_XTOOL_NAME}- || return 0
+source "${CROSSLINUX_SCRIPT_DIR}/_xbt_env_clr"
+cd ..
+
+PKG_STATUS=""
+return 0
+
+}
+
+
+# ******************************************************************************
+# pkg_install
+# ******************************************************************************
+
+pkg_install() {
+
+PKG_STATUS="install error"
+
+cd "${PKG_DIR}"
+source "${CROSSLINUX_SCRIPT_DIR}/_xbt_env_set"
+PATH="${CONFIG_XTOOL_BIN_DIR}:${PATH}" make \
+	CROSS_COMPILE=${CONFIG_XTOOL_NAME}- \
+	DESTDIR=${TARGET_SYSROOT_DIR} \
+	install || return 1
+source "${CROSSLINUX_SCRIPT_DIR}/_xbt_env_clr"
+cd ..
+
+if [[ -d "rootfs/" ]]; then
+	find "rootfs/" ! -type d -exec touch {} \;
+	cp --archive --force rootfs/* "${TARGET_SYSROOT_DIR}"
+fi
+
+PKG_STATUS=""
+return 0
+
+}
+
+
+# ******************************************************************************
+# pkg_clean
+# ******************************************************************************
+
+pkg_clean() {
+PKG_STATUS=""
+return 0
+}
+
+
+# end of file
